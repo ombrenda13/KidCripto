7b4ec4e844f4948ad8094c16d4f9e93ccb4f400f
ombrenda13 <36515687+ombrenda13@users.noreply.github.com>
2020-03-29 02:24:59 -0600

    Update KidCripto.py

--- KidCripto.py
+++ KidCripto.py
@@ -4,24 +4,30 @@
 # In[18]:
 
 
-a=input()
-b=input()
-A=input()
-B=input()
-msg_x=input()
+ED=input()
+a=int(input())
+b=int(input())
+A=int(input())
+B=int(input())
+msg_x=int(input())
+
 M=(a*b)-1
 e=(A*M)+a
 d=(B*M)+b
 n=((e*d)-1)/M
 
-
-encrypt=(msg_x*e)
-cypher_y=float(encrypt%n)
-
-decrypt=cypher_y*d
+encrypt=int((msg_x*e)%n)
+decrypt=(encrypt*d)
 plaintext=(decrypt%n)
 
-print(cypher_y)
+if ED=='E':
+    print(encrypt)
+else:
+    print(plaintext)
+    
+
+
+
 
 
 
